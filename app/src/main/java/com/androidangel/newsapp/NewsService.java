05e17d1f7f7115cc756bcf6b98f6a0fbb624fda3
iamangelauditor <iamangelauditor@gmail.com>
2018-05-12 19:40:46 +0800

    first commit

--- app/src/main/java/com/androidangel/newsapp/NewsService.java
+++ app/src/main/java/com/androidangel/newsapp/NewsService.java
@@ -8,9 +8,9 @@ public interface NewsService {
 
     String BASE_URL = "https://content.guardianapis.com";
 
-    @GET("/search?api-key=9271ca86-4372-430a-9c6f-b9bb1b837446")
+    @GET("/search?api-key=apikey")
     Call<NewsData> listOfAllNews();
 
-    @GET("/sections?api-key=9271ca86-4372-430a-9c6f-b9bb1b837446")
+    @GET("/sections?api-key=apikey")
     Call<SectionData> listSections();
 }
